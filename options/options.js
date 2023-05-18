5fff795aa96f66c1b2c03877375769f13ca75a91
Crowdbotics <team@crowdbotics.com>
2023-05-18 21:30:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "holy-hall-40831",
+  url: "https://holy-hall-40831.botics.co",
+  api: "https://holy-hall-40831.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
